ca2795c9c898269d0c80607053641e887cb163e9
SajjadAlizadehMoghadam <sajjadalizadeh6025@gmail.com>
2024-01-25 22:51:43 +0330

    Add project files.

---
+++ LoginWhiteSQL/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginWhiteSQL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly UserContext _userContext;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _userContext = new UserContext("AppData");
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void mimi_btn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void close_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void log_btn_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txt_box.Text;
+            string password = pass_box.Password;
+
+            var user = _userContext.Users.FirstOrDefault(t => t.UserFullname == username && t.UserPassword == password);
+            if(user != null) {
+                
+                MainWindow main = new MainWindow();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+    }
+}
+
---
+++ LoginWhiteSQL/UserContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginWhiteSQL
+{
+    internal class UserContext : DbContext
+    {
+        public UserContext(string name) : base(name) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(key => key.UserId);
+            modelBuilder.Entity<User>().Property(t => t.UserFullname).IsRequired().HasMaxLength(30).HasColumnName("Username");
+            modelBuilder.Entity<User>().Property(t => t.UserPassword).IsRequired().HasMaxLength(10).HasColumnName("Password");
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+
+
+    internal class User
+    {
+        public int UserId { get; set; }
+        public string UserFullname { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
